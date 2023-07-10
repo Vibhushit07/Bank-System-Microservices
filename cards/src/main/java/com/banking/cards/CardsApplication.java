6324bb609ffcd795a7beb0972e79f1c55c3434d9
Vibhushit07 <vibhushitsinghal80@gmail.com>
2023-07-11 02:01:31 +0530

    created customer model

--- cards/src/main/java/com/banking/cards/CardsApplication.java
+++ cards/src/main/java/com/banking/cards/CardsApplication.java
@@ -2,8 +2,15 @@ package com.banking.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScans({ @ComponentScan("com.banking.cards.controller") })
+@EnableJpaRepositories("com.banking.cards.repository")
+@EntityScan("com.banking.cards.model")
 public class CardsApplication {
 
 	public static void main(String[] args) {
