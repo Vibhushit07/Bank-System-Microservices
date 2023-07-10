bbf8f142c78e988b754980297815fbb704bbf571
Vibhushit07 <vibhushitsinghal80@gmail.com>
2023-07-11 01:42:03 +0530

    created loans

--- loans/src/main/java/com/banking/loans/controller/LoansController.java
+++ loans/src/main/java/com/banking/loans/controller/LoansController.java
@@ -1,4 +1,24 @@
 package com.banking.loans.controller;
 
+import com.banking.loans.model.Customer;
+import com.banking.loans.model.Loans;
+import com.banking.loans.repository.LoansRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class LoansController {
+    @Autowired
+    private LoansRepository loansRepository;
+
+    @PostMapping("/myLoans")
+    public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+        List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+
+        return loans;
+    }
 }
