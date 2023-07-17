e6a58a1a7c0ff2b2974c32a3846a9e0e466b47e7
Vibhushit07 <vibhushitsinghal80@gmail.com>
2023-07-18 00:08:48 +0530

    temp

--- cards/src/main/java/com/banking/cards/CardsApplication.java
+++ cards/src/main/java/com/banking/cards/CardsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+// https://github.com/eazybytes/microservices-with-spring-sectionwise-code
 @SpringBootApplication
 @ComponentScans({ @ComponentScan("com.banking.cards.controller") })
 @EnableJpaRepositories("com.banking.cards.repository")
