82aee3619d23db0016e77db65208dddef763060e
Vibhushit <vibhushitsinghal80@gmail.com>
2023-08-26 01:36:13 +0530

    created config service for cards

--- cards/src/main/java/com/banking/cards/controller/CardsController.java
+++ cards/src/main/java/com/banking/cards/controller/CardsController.java
@@ -1,9 +1,15 @@
 package com.banking.cards.controller;
 
+import com.banking.cards.config.CardsServiceConfig;
 import com.banking.cards.model.Cards;
 import com.banking.cards.model.Customer;
+import com.banking.cards.model.Properties;
 import com.banking.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +22,23 @@ public class CardsController {
     @Autowired
     private CardsRepository cardsRepository;
 
+    @Autowired
+    private CardsServiceConfig cardsServiceConfig;
+
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
         List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
 
         return cards;
+    }
+
+    @GetMapping("/card/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsServiceConfig.getMsg(), cardsServiceConfig.getBuildVersion(),
+                cardsServiceConfig.getMailDetails(), cardsServiceConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
