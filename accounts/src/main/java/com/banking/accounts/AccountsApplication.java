6654e8e337db9eede85027a8dc996ffcc2b346d0
Vibhushit <vibhushitsinghal80@gmail.com>
2023-08-28 00:47:04 +0530

    added refresh scope for all microservices

--- accounts/src/main/java/com/banking/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/banking/accounts/AccountsApplication.java
@@ -3,12 +3,14 @@ package com.banking.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // https://www.educba.com/docker-push/
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.banking.accounts.controller")})
 @EnableJpaRepositories("com.banking.accounts.repository")
 @EntityScan("com.banking.accounts.model")
