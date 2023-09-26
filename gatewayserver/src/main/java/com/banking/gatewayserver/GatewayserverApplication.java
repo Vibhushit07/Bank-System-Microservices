5f93aa4eb0f57fb567cf92a962ff58ae3bb23541
Vibhushit <vibhushitsinghal80@gmail.com>
2023-09-26 23:11:16 +0530

    initialised accounts microservice

--- gatewayserver/src/main/java/com/banking/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/banking/gatewayserver/GatewayserverApplication.java
@@ -1,38 +1,38 @@
-package com.banking.gatewayserver;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Date;
-
-@SpringBootApplication
-public class GatewayserverApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/banking/accounts/**")
-						.filters(f -> f.rewritePath("/banking/accounts/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time",new Date().toString()))
-						.uri("lb://accounts")).
-				route(p -> p
-						.path("/banking/loans/**")
-						.filters(f -> f.rewritePath("/banking/loans/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time",new Date().toString()))
-						.uri("lb://loans")).
-				route(p -> p
-						.path("/banking/cards/**")
-						.filters(f -> f.rewritePath("/banking/cards/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time",new Date().toString()))
-						.uri("lb://cards")).build();
-	}
-
-}
+//package com.banking.gatewayserver;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//
+//import java.util.Date;
+//
+//@SpringBootApplication
+//public class GatewayserverApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(GatewayserverApplication.class, args);
+//	}
+//
+//	@Bean
+//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route(p -> p
+//						.path("/banking/accounts/**")
+//						.filters(f -> f.rewritePath("/banking/accounts/(?<segment>.*)","/${segment}")
+//								.addResponseHeader("X-Response-Time",new Date().toString()))
+//						.uri("lb://accounts")).
+//				route(p -> p
+//						.path("/banking/loans/**")
+//						.filters(f -> f.rewritePath("/banking/loans/(?<segment>.*)","/${segment}")
+//								.addResponseHeader("X-Response-Time",new Date().toString()))
+//						.uri("lb://loans")).
+//				route(p -> p
+//						.path("/banking/cards/**")
+//						.filters(f -> f.rewritePath("/banking/cards/(?<segment>.*)","/${segment}")
+//								.addResponseHeader("X-Response-Time",new Date().toString()))
+//						.uri("lb://cards")).build();
+//	}
+//
+//}
