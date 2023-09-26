cdaaf04ff34cda9551c4702c29e4122786121641
Vibhushit <vibhushitsinghal80@gmail.com>
2023-09-26 23:51:02 +0530

    created accounts repository for accounts microservice

--- accounts/src/main/java/com/banking/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/banking/accounts/repository/AccountsRepository.java
@@ -1,11 +1,11 @@
-//package com.banking.accounts.repository;
-//
-//import com.banking.accounts.model.Accounts;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface AccountsRepository extends CrudRepository<Accounts, Long> {
-//
-//    Accounts findByCustomerId(int customerId);
-//}
+package com.banking.accounts.repository;
+
+import com.banking.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+
+    Accounts findByCustomerId(int customerId);
+}
