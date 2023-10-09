403aa3d398eee499863f675b3ef90bfed8387391
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-09 23:06:48 +0530

    added update Account method in service interface

--- accounts/src/main/java/com/banking/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/banking/accounts/service/IAccountsService.java
@@ -16,4 +16,11 @@ public interface IAccountsService {
      * @return Accounts Details based on a given mobileNumber
      */
     CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     * @return boolean indicating if the update of the Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
 }
