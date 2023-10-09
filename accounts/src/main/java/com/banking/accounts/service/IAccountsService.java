5ce6acd74742df0229609b12cd18ce306bfff136
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-09 21:47:17 +0530

    created account service interface for accounts microservice

---
+++ accounts/src/main/java/com/banking/accounts/service/IAccountsService.java
@@ -0,0 +1,12 @@
+package com.banking.accounts.service;
+
+import com.banking.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     */
+    void createAccount(CustomerDto customerDto);
+}
