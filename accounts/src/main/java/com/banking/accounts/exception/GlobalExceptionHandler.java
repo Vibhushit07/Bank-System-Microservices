01b97d0003c51e31575f1c7f1e8f195182c0f0a7
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-09 22:23:53 +0530

    created common exception handling class

--- accounts/src/main/java/com/banking/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/banking/accounts/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.banking.accounts.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception,
                                                                                  WebRequest webRequest) {
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(
