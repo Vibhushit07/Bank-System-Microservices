a079b8c8da6b7bd6c5ef48226ddb9e8b794a74d9
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-09 22:49:31 +0530

    update account repo for fetch account details

--- accounts/src/main/java/com/banking/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/banking/accounts/repository/AccountsRepository.java
@@ -4,8 +4,10 @@ import com.banking.accounts.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 
-    Accounts findByCustomerId(int customerId);
+    Optional<Accounts> findByCustomerId(Long customerId);
 }
