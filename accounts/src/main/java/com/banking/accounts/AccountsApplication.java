ceccd054b9a69ae6ba1949f550d556995a8f4d00
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-09 20:49:54 +0530

    removed unwanted code for accounts microservice

--- accounts/src/main/java/com/banking/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/banking/accounts/AccountsApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 // https://github.com/spring-cloud/spring-cloud-stream/issues/2639
 @SpringBootApplication
-//@RefreshScope
-//@EnableFeignClients
-//@ComponentScans({ @ComponentScan("com.banking.accounts.controller")})
-//@EnableJpaRepositories("com.banking.accounts.repository")
-//@EntityScan("com.banking.accounts.model")
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/com/banking/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/banking/accounts/controller/AccountsController.java
@@ -1,10 +1,7 @@
 package com.banking.accounts.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountsController {
@@ -13,73 +10,4 @@ public class AccountsController {
     public String sayHello() {
         return "Hello World";
     }
-
-//    @Autowired
-//    private AccountsRepository accountsRepository;
-
-//    @Autowired
-//    private AccountsServiceConfig accountsServiceConfig;
-
-//    @Autowired
-//    LoansFeignClient loansFeignClient;
-
-//    @Autowired
-//    CardsFeignClient cardsFeignClient;
-
-//    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
-
-
-//    @PostMapping("/myAccount")
-//    public Accounts getAccountDetail(@RequestBody Customer customer) {
-//
-//        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-//        return accounts;
-//    }
-
-//    @GetMapping("/account/properties")
-//    public String getPropertyDetails() throws JsonProcessingException {
-//        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-//        Properties properties = new Properties(accountsServiceConfig.getMsg(), accountsServiceConfig.getBuildVersion(),
-//                accountsServiceConfig.getMailDetails(), accountsServiceConfig.getActiveBranches());
-//        String jsonStr = ow.writeValueAsString(properties);
-//        return jsonStr;
-//    }
-
-//    @PostMapping("/myCustomerDetails")
-//    @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallback")
-//    @Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallback")
-//    public CustomerDetails myCustomerDetails(@RequestHeader("banking-correlation-id") String correlationId, @RequestBody Customer customer) {
-//        logger.info("myCustomerDetials() started");
-//        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-//        List<Loans> loans = loansFeignClient.getLoanDetails(correlationId, customer);
-//        List<Cards> cards = cardsFeignClient.getCardDetails(correlationId, customer);
-//        logger.info("myCustomerDetials() ended");
-//        return CustomerDetails
-//                .builder()
-//                .accounts(accounts)
-//                .loans(loans)
-//                .cards(cards)
-//                .build();
-//    }
-
-//    private CustomerDetails myCustomerDetailsFallback(@RequestHeader("banking-correlation-id") String correlationId, Customer customer, Throwable t) {
-//        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-//        List<Loans> loans = loansFeignClient.getLoanDetails(correlationId, customer);
-//
-//        return CustomerDetails
-//                .builder()
-//                .accounts(accounts)
-//                .loans(loans)
-//                .build();
-//    }
-
-//    @GetMapping("/sayHello")
-//    @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallbackMethod")
-//    public String sayHello() {
-//        return "Hello, Welcome to Banking Microservice Application";
-//    }
-
-//    private String sayHelloFallbackMethod(Throwable t) {
-//        return "Hi, Welcome to Banking Microservice Application";
-//    }
 }
