1fa4322dfc433bf4afc0b8c5f9a00318e3ccb1c2
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-10 23:42:29 +0530

    validation for account and customer details

--- accounts/src/main/java/com/banking/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/banking/accounts/dto/AccountsDto.java
@@ -1,13 +1,19 @@
 package com.banking.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "Account Number can not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account Number must be of 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account Type can not be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch Address can not be null or empty")
     private String branchAddress;
 }
