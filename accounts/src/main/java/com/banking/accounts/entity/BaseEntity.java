964e96742f8edbff5572018a8dba12a4df28ed4c
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-10 23:58:47 +0530

    fixed created and updated details

--- accounts/src/main/java/com/banking/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/banking/accounts/entity/BaseEntity.java
@@ -3,6 +3,10 @@ package com.banking.accounts.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -15,15 +19,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BaseEntity {
 
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 }
