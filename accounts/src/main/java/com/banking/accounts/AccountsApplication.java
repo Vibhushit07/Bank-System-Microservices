d7d6e7a47ff6e84997c31c241b89b4207d70e877
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-11 00:08:46 +0530

    fixed created and updated details

--- accounts/src/main/java/com/banking/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/banking/accounts/AccountsApplication.java
@@ -2,11 +2,13 @@ package com.banking.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // https://www.educba.com/docker-push/
 // docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 // https://github.com/spring-cloud/spring-cloud-stream/issues/2639
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication {
 
 	public static void main(String[] args) {
