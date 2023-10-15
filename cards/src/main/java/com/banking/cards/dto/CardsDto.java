27d9f635130400772dc755492332e51a47b37d56
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-15 21:18:28 +0530

    added validation for cards dto for cards Microservice

--- cards/src/main/java/com/banking/cards/dto/CardsDto.java
+++ cards/src/main/java/com/banking/cards/dto/CardsDto.java
@@ -1,14 +1,31 @@
 package com.banking.cards.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class CardsDto {
 
+    @NotEmpty(message = "Mobile Number can not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be of 10 digits")
     private String mobileNumber;
+
+    @NotEmpty(message = "Card Number can not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{12})", message = "Card number must be of 12 digits")
     private String cardNumber;
+
+    @NotEmpty(message = "Card Type can not be null or empty")
     private String cardType;
+
+    @Positive(message = "Total card limit should be greater than zero")
     private int totalLimit;
+
+    @PositiveOrZero(message = "Total amount used should be equal or greater than zero")
     private int amountUsed;
+
+    @PositiveOrZero(message = "Total available amount should be equal or greater than zero")
     private int availableAmount;
 }
