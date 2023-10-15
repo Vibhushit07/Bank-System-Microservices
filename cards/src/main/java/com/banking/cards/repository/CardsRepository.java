21358d8bd199f18afbb5c650d0383520aa1764bf
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-15 21:32:02 +0530

    created repo interface for cards Microservice

--- cards/src/main/java/com/banking/cards/repository/CardsRepository.java
+++ cards/src/main/java/com/banking/cards/repository/CardsRepository.java
@@ -1,16 +1,16 @@
-//package com.banking.cards.repository;
-//
-//
-//import com.banking.cards.model.Cards;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//public interface CardsRepository extends CrudRepository<Cards, Long> {
-//
-//
-//    List<Cards> findByCustomerId(int customerId);
-//
-//}
+package com.banking.cards.repository;
+
+
+import com.banking.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+}
