baf7b785569e5bfc335e9e41f2847ba6031ec373
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-15 21:29:17 +0530

    created create card service interface for cards Microservice

---
+++ cards/src/main/java/com/banking/cards/service/ICardsService.java
@@ -0,0 +1,10 @@
+package com.banking.cards.service;
+
+public interface ICardsService {
+
+    /**
+     *
+     * @param mobileNumber- Mobile Number of Customer
+     */
+    void createCard(String mobileNumber);
+}
