468a3372da6983573a04ce1534db2fbe7c4a0156
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-15 21:12:30 +0530

    created Cards Dto for cards Microservice

---
+++ cards/src/main/java/com/banking/cards/dto/CardsDto.java
@@ -0,0 +1,14 @@
+package com.banking.cards.dto;
+
+import lombok.Data;
+
+@Data
+public class CardsDto {
+
+    private String mobileNumber;
+    private String cardNumber;
+    private String cardType;
+    private int totalLimit;
+    private int amountUsed;
+    private int availableAmount;
+}
