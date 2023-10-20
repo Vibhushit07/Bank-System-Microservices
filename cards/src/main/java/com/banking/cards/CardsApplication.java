9f89ace36683d35b22af33d6ee4f865c764bca66
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-20 22:41:20 +0530

    added overview of cards microservice

--- cards/src/main/java/com/banking/cards/CardsApplication.java
+++ cards/src/main/java/com/banking/cards/CardsApplication.java
@@ -1,11 +1,36 @@
 package com.banking.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Card microservice REST API Documentation",
+				description = "Bank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Vibhushit",
+						email = "NA",
+						url = "NA"
+				),
+				license = @License(
+						name = "NA",
+						url = "NA"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Cards microservice REST API documentation",
+				url = "NA"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
