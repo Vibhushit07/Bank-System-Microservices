9782420198f5de6ab5af6464b2bd49baa6dcd52d
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-21 01:39:51 +0530

    created Loan Microservice

--- loans/src/main/java/com/banking/loans/LoansApplication.java
+++ loans/src/main/java/com/banking/loans/LoansApplication.java
@@ -1,6 +1,10 @@
 package com.banking.loans;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LoansApplication {
 
 	public static void main(String[] args) {
