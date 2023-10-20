64ae7c007af61f7ea1c90fc12ed6212507e85c43
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-21 01:56:46 +0530

    created at, by and update at and by will be set by annotation

--- loans/src/main/java/com/banking/loans/LoansApplication.java
+++ loans/src/main/java/com/banking/loans/LoansApplication.java
@@ -3,8 +3,10 @@ package com.banking.loans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansApplication {
 
 	public static void main(String[] args) {
