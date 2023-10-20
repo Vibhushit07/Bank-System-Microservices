f6b73f3465dc88818d56919574100fae7153abfb
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-20 16:14:02 +0530

    implemented create new card

--- cards/src/main/java/com/banking/cards/controller/CardsController.java
+++ cards/src/main/java/com/banking/cards/controller/CardsController.java
@@ -1,10 +1,12 @@
 package com.banking.cards.controller;
 
 
-import com.banking.cards.dto.CardsDto;
+import com.banking.cards.constants.CardsConstants;
 import com.banking.cards.dto.ResponseDto;
+import com.banking.cards.service.ICardsService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,8 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class CardsController {
 
-    @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createCards(@Valid @RequestBody CardsDto cardsDto) {
+    private ICardsService iCardsService;
 
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createCards(@Valid @RequestBody String mobileNumber) {
+        iCardsService.createCard(mobileNumber);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
     }
 }
