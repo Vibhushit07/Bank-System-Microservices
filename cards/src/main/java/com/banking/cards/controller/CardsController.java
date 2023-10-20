ad2c4a822024ff823cd3e221c4419a8aa3acf541
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-20 21:27:37 +0530

    checked whether mobile number is valid or not

--- cards/src/main/java/com/banking/cards/controller/CardsController.java
+++ cards/src/main/java/com/banking/cards/controller/CardsController.java
@@ -5,14 +5,15 @@ import com.banking.cards.constants.CardsConstants;
 import com.banking.cards.dto.ResponseDto;
 import com.banking.cards.service.ICardsService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,7 +25,7 @@ public class CardsController {
     private ICardsService iCardsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createCards(@Valid @RequestBody String mobileNumber) {
+    public ResponseEntity<ResponseDto> createCards(@Valid @RequestParam @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits") String mobileNumber) {
         iCardsService.createCard(mobileNumber);
 
         return ResponseEntity
