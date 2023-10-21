b69977bc19c59746300b6d6d2308853aa65826a4
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-21 11:26:20 +0530

    mapper for loans dto

--- loans/src/main/java/com/banking/loans/mapper/LoansMapper.java
+++ loans/src/main/java/com/banking/loans/mapper/LoansMapper.java
@@ -5,7 +5,7 @@ import com.banking.loans.entity.Loans;
 
 public class LoansMapper {
 
-    public static LoansDto mapToLoanssDto(Loans loans, LoansDto loansDto) {
+    public static LoansDto mapToLoansDto(Loans loans, LoansDto loansDto) {
 
         loansDto.setLoanType(loans.getLoanType());
         loansDto.setTotalLoan(loans.getTotalLoan());
