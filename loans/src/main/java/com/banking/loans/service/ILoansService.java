8d28d2dfc2bff2245264493f85f701a817bcd969
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-21 11:16:33 +0530

    added fetch loan method in service interface

--- loans/src/main/java/com/banking/loans/service/ILoansService.java
+++ loans/src/main/java/com/banking/loans/service/ILoansService.java
@@ -1,5 +1,7 @@
 package com.banking.loans.service;
 
+import com.banking.loans.dto.LoansDto;
+
 public interface ILoansService {
 
     /**
@@ -7,4 +9,11 @@ public interface ILoansService {
      * @param mobileNumber- Mobile Number of Customer
      */
     void createLoan(String mobileNumber);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Loan Details based on a given mobileNumber
+     */
+    LoansDto fetchLoan(String mobileNumber);
 }
