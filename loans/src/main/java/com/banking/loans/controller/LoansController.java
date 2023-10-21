034ad71086970d9b3e82228a62d62936997e1740
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-21 11:57:46 +0530

    implemented create loan controller

--- loans/src/main/java/com/banking/loans/controller/LoansController.java
+++ loans/src/main/java/com/banking/loans/controller/LoansController.java
@@ -1,7 +1,32 @@
 package com.banking.loans.controller;
 
 
+import com.banking.loans.constants.LoansConstants;
+import com.banking.loans.dto.ResponseDto;
+import com.banking.loans.service.ILoansService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+@Validated
 public class LoansController {
 
+    private ILoansService iLoansService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createCards(@RequestParam String mobileNumber) {
+        iLoansService.createLoan(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
+    }
 }
