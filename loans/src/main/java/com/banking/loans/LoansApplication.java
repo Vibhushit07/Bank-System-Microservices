17dbf7e2b2b84b59289953bc1321e1a98d1c7d2b
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-21 12:49:38 +0530

    description of Loan Microservices

--- loans/src/main/java/com/banking/loans/LoansApplication.java
+++ loans/src/main/java/com/banking/loans/LoansApplication.java
@@ -1,12 +1,37 @@
 package com.banking.loans;
 
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loan microservice REST API Documentation",
+				description = "Bank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Vibhushit",
+						email = "NA",
+						url = "NA"
+				),
+				license = @License(
+						name = "NA",
+						url = "NA"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Cards microservice REST API documentation",
+				url = "NA"
+		)
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
