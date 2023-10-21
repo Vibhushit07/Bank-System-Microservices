6394fb365b92e983cfbae14735ac28bfc58a64fc
Vibhushit <vibhushitsinghal80@gmail.com>
2023-10-21 11:15:44 +0530

    added create loan method in service interface

---
+++ loans/src/main/java/com/banking/loans/service/ILoansService.java
@@ -0,0 +1,10 @@
+package com.banking.loans.service;
+
+public interface ILoansService {
+
+    /**
+     *
+     * @param mobileNumber- Mobile Number of Customer
+     */
+    void createLoan(String mobileNumber);
+}
