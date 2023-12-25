73119cd0736d4c408daab053c7d93c29718d12dc
Vibhushit <vibhushitsinghal80@gmail.com>
2023-12-25 23:40:29 +0530

    contacts configuration in application in loans microservice

--- loans/src/main/java/com/banking/loans/LoansApplication.java
+++ loans/src/main/java/com/banking/loans/LoansApplication.java
@@ -1,6 +1,7 @@
 package com.banking.loans;
 
 
+import com.banking.loans.dto.LoansContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -8,10 +9,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Loan microservice REST API Documentation",
