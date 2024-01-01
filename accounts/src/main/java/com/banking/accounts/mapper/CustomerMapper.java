00e2804914ab97c2fdb8de3565e3950cff731812
Vibhushit <vibhushitsinghal80@gmail.com>
2024-01-02 01:56:23 +0530

    created customer details mapper from accounts microservice

--- accounts/src/main/java/com/banking/accounts/mapper/CustomerMapper.java
+++ accounts/src/main/java/com/banking/accounts/mapper/CustomerMapper.java
@@ -1,5 +1,6 @@
 package com.banking.accounts.mapper;
 
+import com.banking.accounts.dto.CustomerDetailsDto;
 import com.banking.accounts.dto.CustomerDto;
 import com.banking.accounts.entity.Customer;
 
@@ -10,6 +11,13 @@ public class CustomerMapper {
         customerDto.setEmail(customer.getEmail());
         customerDto.setMobileNumber(customer.getMobileNumber());
         return customerDto;
+    }
+
+    public static CustomerDetailsDto mapToCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDto) {
+        customerDetailsDto.setName(customer.getName());
+        customerDetailsDto.setEmail(customer.getEmail());
+        customerDetailsDto.setMobileNumber(customer.getMobileNumber());
+        return customerDetailsDto;
     }
 
     public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
