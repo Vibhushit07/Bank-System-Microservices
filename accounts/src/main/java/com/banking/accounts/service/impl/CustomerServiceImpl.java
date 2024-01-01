47d05c74f23b29fc0cfdaac3384dcd8353184af7
Vibhushit <vibhushitsinghal80@gmail.com>
2024-01-02 01:56:52 +0530

    implemented fetch customer details mapper in accounts microservice

---
+++ accounts/src/main/java/com/banking/accounts/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,54 @@
+package com.banking.accounts.service.impl;
+
+import com.banking.accounts.dto.AccountsDto;
+import com.banking.accounts.dto.CardsDto;
+import com.banking.accounts.dto.CustomerDetailsDto;
+import com.banking.accounts.dto.LoansDto;
+import com.banking.accounts.entity.Accounts;
+import com.banking.accounts.entity.Customer;
+import com.banking.accounts.exception.ResourceNotFoundException;
+import com.banking.accounts.mapper.AccountsMapper;
+import com.banking.accounts.mapper.CustomerMapper;
+import com.banking.accounts.repository.AccountsRepository;
+import com.banking.accounts.repository.CustomerRepository;
+import com.banking.accounts.service.ICustomerService;
+import com.banking.accounts.service.client.CardsFeignClient;
+import com.banking.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerServiceImpl implements ICustomerService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on given mobile number
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobile number", mobileNumber)
+        );
+
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        CardsDto cardsDto = cardsFeignClient.fetchCardDetails(mobileNumber).getBody();
+        LoansDto loansDto = loansFeignClient.fetchLoanDetails(mobileNumber).getBody();
+
+        customerDetailsDto.setCardsDto(cardsDto);
+        customerDetailsDto.setLoansDto(loansDto);
+
+        return customerDetailsDto;
+    }
+}
