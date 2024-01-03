ca1ee56cd9b6c4b607df03a571b3582b5ad02980
Vibhushit <vibhushitsinghal80@gmail.com>
2024-01-04 01:51:15 +0530

    added dynamic urls in gateway server

--- gatewayserver/src/main/java/com/banking/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/banking/gatewayserver/GatewayserverApplication.java
@@ -2,11 +2,32 @@ package com.banking.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator bankingRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(path -> path
+						.path("/banking/accounts/**")
+						.filters(filter -> filter.rewritePath("/banking/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))  // lb means Load Balancer
+				.route(path -> path
+						.path("/banking/cards/**")
+						.filters(filter -> filter.rewritePath("/banking/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.route(path -> path
+						.path("/banking/loans/**")
+						.filters(filter -> filter.rewritePath("/banking/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.build();
 	}
 }
