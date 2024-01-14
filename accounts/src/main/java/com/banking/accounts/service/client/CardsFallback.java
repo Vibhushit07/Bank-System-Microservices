6faa0075e2a3dd43707268e8c88c963d63c273a4
Vibhushit <vibhushitsinghal80@gmail.com>
2024-01-15 00:07:49 +0530

    implemented fallback for cards in accounts microservice

---
+++ accounts/src/main/java/com/banking/accounts/service/client/CardsFallback.java
@@ -0,0 +1,19 @@
+package com.banking.accounts.service.client;
+
+import com.banking.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient {
+
+    /**
+     * @param correlationId
+     * @param mobileNumber
+     * @return
+     */
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
