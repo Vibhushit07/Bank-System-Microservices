b88e913061b4b27ee4508ce39ab65004b77b39b0
Vibhushit <vibhushitsinghal80@gmail.com>
2024-01-21 13:57:53 +0530

    added fallback method for redis rate limiter in accounts microservice

--- accounts/src/main/java/com/banking/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/banking/accounts/controller/AccountsController.java
@@ -225,10 +225,14 @@ public class AccountsController {
                     )
             )
     })
-    @RateLimiter(name = "getJavaVersion")
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity.status(HttpStatus.OK).body("Java 17");
     }
 
     @Operation(
